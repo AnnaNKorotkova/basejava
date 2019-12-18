198eeebe7e5bd2099d1fd03a8ad0b0345fbb3e04
AnnaNKorotkova <korotkova.a.n@yandex.ru>
2019-12-18 21:09:02 +0300

    Реализация хранилища резюме в массиве 2.0

--- src/com/basejava/ArrayStorage.java
+++ src/com/basejava/ArrayStorage.java
@@ -3,66 +3,59 @@ package com.basejava;
 import java.util.Arrays;
 
 public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
 
-    private Resume[] arrResume;
-    private int size;
-    private int lastUuid;
+    void clear() {
+        Arrays.fill(storage, null);
 
-    public ArrayStorage(Resume[] arrResume) {
-        this.arrResume = arrResume;
     }
 
-    public void get(Resume resume) {
-        if (resume == null) {
-            System.out.println("Запршенного резюме не существует.");
-        } else System.out.println(resume.toString());
+    void save(Resume r) {
+        storage[size()] = r;
+        System.out.println("Резюме с id = " + r.uuid + " создано.");
     }
 
-    public void getAll(Resume[] arrResume) {
-        for (int i = 0; i < size; i++) {
-            get(arrResume[i]);
-        }
-    }
-
-    public void save(Resume[] arrResume) {
-        Resume resume = new Resume();
-        resume.enterResume();
-        lastUuid += 1;
-        resume.setUuid(lastUuid);
-        arrResume[size] = resume;
-        size += 1;
-        System.out.println("Резюме  id=" + lastUuid + ", создано.");
-    }
-
-    public void delete(int index) {
-        if (size > index) {
-            int id = arrResume[index].getUuid();
-            for (int k = index; k < size - 1; k++) {
-                arrResume[k] = arrResume[k + 1];
-            }
-            arrResume[size - 1] = null;
-            size -= 1;
-            System.out.println("Резюме id=" + id + ", с индексом: " + index + ", удалено.");
-        } else {
-            System.out.println("Невозможно удалить резюме с индексом: " + index + ", такого резюме не существует.");
-        }
-    }
-
-    public void clear(Resume[] arrResume) {
-        Arrays.fill(arrResume, null);
-        size = 0;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public String findIndexByUuid(int uuid) {
-        for (int i = 0; i < size; i++) {
-            if (arrResume[i].getUuid() == uuid) {
-                return String.valueOf(i);
+    Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
             }
         }
-        return "Резюме с таким id:" + uuid + "не найдено";
+        System.out.println("Невозможно найти резюме с id = " + uuid + ", такого резюме не существует.");
+        return null;
+    }
+
+    void delete(String uuid) {
+        int index = 0;
+        for (int i = 0; i < size(); i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                index = i;
+                for (int k = index; k < size() - 1; k++) {
+                    storage[k] = storage[k + 1];
+                }
+                storage[size() - 1] = null;
+                System.out.println("Резюме с id = " + uuid + " удалено.");
+                break;
+            } else {
+                System.out.println("Невозможно удалить резюме с id = " + uuid + ", такого резюме не существует.");
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        return Arrays.copyOf(storage, size());
+    }
+
+    int size() {
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
--- src/com/basejava/Resume.java
+++ src/com/basejava/Resume.java
@@ -1,113 +1,11 @@
 package com.basejava;
 
-import java.util.Scanner;
-
 public class Resume {
 
-    private String name;
-    private String birthday;
-    private String education;
-    private String jobs;
-    private String ability;
-    private String contactPhone;
-    private String contactEmail;
-    private int uuid;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getEducation() {
-        return education;
-    }
-
-    public void setEducation(String education) {
-        this.education = education;
-    }
-
-    public String getJobs() {
-        return jobs;
-    }
-
-    public void setJobs(String jobs) {
-        this.jobs = jobs;
-    }
-
-    public String getAbility() {
-        return ability;
-    }
-
-    public void setAbility(String ability) {
-        this.ability = ability;
-    }
-
-    public String getContactPhone() {
-        return contactPhone;
-    }
-
-    public void setContactPhone(String contactPhone) {
-        this.contactPhone = contactPhone;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
-    public int getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(int uuid) {
-        this.uuid = uuid;
-    }
-
-    Scanner scan = new Scanner(System.in);
-
-    public void enterResume() {
-        System.out.println("Заполнение резюме");
-        System.out.print("Введите имя: ");
-        setName(scan.next());
-        System.out.print("Дата рождения: ");
-        setBirthday(scan.next());
-        System.out.print("Образование: ");
-        setEducation(scan.next());
-        System.out.print("Опыт работы: ");
-        setJobs(scan.next());
-        System.out.print("Личные качества: ");
-        setAbility(scan.next());
-        System.out.print("Контактный телефон: ");
-        setContactPhone(scan.next());
-        System.out.print("E-mail: ");
-        setContactEmail(scan.next());
-    }
+     String uuid;
 
     @Override
     public String toString() {
-        return "Resume{" +
-                "id = '" + uuid + '\'' +
-                ", Имя = '" + name + '\'' +
-                ", Дата рождения = " + birthday +
-                ", Образование = '" + education + '\'' +
-                ", Опыт работы = '" + jobs + '\'' +
-                ", Личные качества = '" + ability + '\'' +
-                ", Телефон ='" + contactPhone + '\'' +
-                ", E-mail = '" + contactEmail + '\'' +
-                '}';
+        return uuid;
     }
 }
