e00688878c70606b29a5bc39620fe11b7c5ef039
AnnaNKorotkova <korotkova.a.n@yandex.ru>
2019-12-19 08:15:33 +0300

    Реализация хранилища резюме в массиве 2.1

--- src/com/basejava/ArrayStorage.java
+++ src/com/basejava/ArrayStorage.java
@@ -4,19 +4,22 @@ import java.util.Arrays;
 
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
-        Arrays.fill(storage, null);
-
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        size++;
         System.out.println("Резюме с id = " + r.uuid + " создано.");
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 return storage[i];
             }
@@ -27,13 +30,14 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int index = 0;
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 index = i;
-                for (int k = index; k < size() - 1; k++) {
+                for (int k = index; k < size - 1; k++) {
                     storage[k] = storage[k + 1];
                 }
-                storage[size() - 1] = null;
+                storage[size - 1] = null;
+                size--;
                 System.out.println("Резюме с id = " + uuid + " удалено.");
                 break;
             } else {
@@ -46,16 +50,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                count++;
-            }
-        }
-        return count;
+        return size;
     }
 }
