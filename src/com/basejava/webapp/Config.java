0a5b876cab7bb0e06bd985b3e29c4d885853d0b0
AnnaNKorotkova <korotkova.a.n@yandex.ru>
2020-03-04 11:56:58 +0300

    HW15 1.2.2 Изменила Config в рамках разбора ДЗ

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.Properties;
 
 public class Config {
-    protected static final File PROPS = new File("/home/mc/basejava/config/resumes.properties");
+    protected static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
     private static final Config INSTANCE = new Config();
     private final File storageDir;
     private final Storage storage;
@@ -19,26 +19,12 @@ public class Config {
         return INSTANCE;
     }
 
-    public Storage getStorage() {
-        return storage;
-    }
-
-    private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
-            Properties props = new Properties();
-            props.load(is);
-            storageDir = new File(props.getProperty("storage.dir"));
-            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
-            dbUrl = props.getProperty("db.url");
-            dbUser = props.getProperty("db.user");
-            dbPassword = props.getProperty("db.password");
-    } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
-        }
-    }
-
     public File getStorageDir() {
         return storageDir;
+    }
+
+    public Storage getStorage() {
+        return storage;
     }
 
     public String getDbUrl() {
@@ -51,5 +37,28 @@ public class Config {
 
     public String getDbPassword() {
         return dbPassword;
+    }
+
+    private Config() {
+        try (InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
+            props.load(is);
+            storageDir = new File(props.getProperty("storage.dir"));
+            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
+            dbUrl = props.getProperty("db.url");
+            dbUser = props.getProperty("db.user");
+            dbPassword = props.getProperty("db.password");
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+        }
+    }
+
+    private static File getHomeDir() {
+        String props = System.getProperty("homeDir");
+        File homeDir = new File(props == null ? "." : props);
+        if (!homeDir.isDirectory()){
+            throw new IllegalStateException(homeDir + " does't directory");
+        }
+        return homeDir;
     }
 }
